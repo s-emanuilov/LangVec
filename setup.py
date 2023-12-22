e8c65f62e4446964f3a1ab8422af0dc1802c6782
Simeon Emanuilov <simeon.emanuilov@gmail.com>
2023-12-22 09:09:19 +0200

    add setup.py file for PyPi packaging

---
+++ setup.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+
+from setuptools import setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="LoV",
+    version="0.0.1",
+    author="Simeon Emanuilov",
+    author_email="simeon.emanuilov@gmail.com",
+    description="Language of Vectors (LoV) is a simple Python library designed for transforming numerical vector data into a language-like structure using a predefined set of words (lexicon).",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    packages=["lov"],
+    python_requires=">=3.00",
+    install_requires=[
+        "numpy"
+    ],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords="LoV semantic search vectorization",
+)
