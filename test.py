71fe43fd034bc2333d5a34d23065a061415be51e
Simeon Emanuilov <simeon.emanuilov@gmail.com>
2024-04-05 07:44:38 +0300

    version 0.0.2 (WIP) - updated example

--- test.py
+++ test.py
@@ -1,35 +1,33 @@
+import string
 import numpy as np
-
 from langvec import LangVec
 
-# Set a seed for reproducibility
 np.random.seed(42)
 
-# Define the dimensionality of the vectors
-NUM_VECTORS = 1000
-VECTORS_DIMENSIONS = 768
+# Define a new lexicon with lowercase and uppercase letters
+LEXICON = list(string.ascii_letters)
 
-# Generate random vectors using list comprehension
-vectors = [np.random.uniform(0, 1, VECTORS_DIMENSIONS) for _ in range(NUM_VECTORS)]
+# Initialize LangVec with the new lexicon
+lv = LangVec(lexicon=LEXICON)
 
-input_vector = np.random.uniform(0, 1, VECTORS_DIMENSIONS)
+NUM_VECTORS = 10000
+DIMENSIONS = 256
 
-# Initiate object
-lv = LangVec()
+# Generate some random data
+vectors = [np.random.uniform(0, 1, DIMENSIONS) for _ in range(NUM_VECTORS)]
 
-# Fit to get with the distribution
+# Fit to this data (getting to know the distribution)
 lv.fit(vectors)
-lv.save("model.lv")
-lv.info()
-# lv.load("model.lv")
 
-# Predict with summary on some of the vectors (getting the lexicon representation)
-print(lv.predict(input_vector, summarized=True))
+# Example vector for prediction
+input_vector = np.random.uniform(0, 1, DIMENSIONS)
 
-# Change slightly some of the elements of given vector
-input_vector_copy = input_vector
-input_vector_copy[0] += 0.1
-print(lv.predict(input_vector_copy, summarized=True))
+# Make prediction on the unseen vector embedding
+predicted_string = ''.join(lv.predict(input_vector))
+print(predicted_string)
+if len(predicted_string) > 6:
+    summarized_string = ''.join(predicted_string[:3]) + '...' + ''.join(predicted_string[-3:])
+else:
+    summarized_string = ''.join(predicted_string)
 
-# Predict without summary on some of the vectors (getting the lexicon representation)
-print(lv.predict(input_vector, summarized=False))
+print(summarized_string)
